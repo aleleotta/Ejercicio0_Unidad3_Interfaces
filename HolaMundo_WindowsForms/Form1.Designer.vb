d5cb5ea718c58096f616a84d6215de357215d1b3
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-09-29 10:07:22 +0200

    .

--- HolaMundo_WindowsForms/Form1.Designer.vb
+++ HolaMundo_WindowsForms/Form1.Designer.vb
@@ -22,16 +22,52 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Button1 = New Button()
+        txtNombre = New TextBox()
+        Label1 = New Label()
         SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(190, 242)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Saludar"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' txtNombre
+        ' 
+        txtNombre.Location = New Point(190, 193)
+        txtNombre.Name = "txtNombre"
+        txtNombre.Size = New Size(262, 23)
+        txtNombre.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(121, 197)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(54, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "Nombre:"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(789, 420)
+        BackColor = SystemColors.Control
+        ClientSize = New Size(821, 487)
+        Controls.Add(Label1)
+        Controls.Add(txtNombre)
+        Controls.Add(Button1)
         Name = "Form1"
         Text = "<x"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents Label1 As Label
 End Class
--- HolaMundo_WindowsForms/Form1.vb
+++ HolaMundo_WindowsForms/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Hola " + txtNombre.Text + "!")
     End Sub
+
 End Class
